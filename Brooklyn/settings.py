36c8f0ca94ea31ebdc0e6c17001f5017d86a6e53
HVLLOWe3n <Roman.Synovets@gmail.com>
2018-08-20 22:37:33 +0300

    configure postgres database

--- Brooklyn/settings.py
+++ Brooklyn/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Brooklyn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'brooklyn_test',
+        'USER': 'postgres',
+        'PASSWORD': 'Pass12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
