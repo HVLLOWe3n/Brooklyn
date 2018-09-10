3195954ea0099c9e6fa031303765b6df53ca7209
HVLLOWe3n <Roman.Synovets@gmail.com>
2018-09-10 18:38:04 +0300

    Fix FrontEnd - overlay

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,10 +1,13 @@
 $("#menu-toggle").click(function (event) {
     event.preventDefault();
     $("#wrapper").toggleClass("toggled");
-});
 
-$(document).on('click', '.navbar-toggler-icon', function(e){
-  e.preventDefault();
-  $('.overlay').show();
-  $('.overlay').data('element', 'navbar-toggler-icon');
+    if ($('.overlay').attr('data-element') === 'navbar-toggler-icon') {
+        $('.overlay').hide();
+        $('.overlay').attr('data-element', '');
+    } else {
+        $('.overlay').show();
+        $('.overlay').attr('data-element', 'navbar-toggler-icon');
+
+    }
 });
