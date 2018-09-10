ebb92456d0450017e65757c6f999c6599f27b3f6
HVLLOWe3n <roman.synovets@gmail.com>
2018-09-10 18:04:05 +0300

    Fix FrontEnd

--- static/js/functions.js
+++ static/js/functions.js
@@ -2,3 +2,9 @@ $("#menu-toggle").click(function (event) {
     event.preventDefault();
     $("#wrapper").toggleClass("toggled");
 });
+
+$(document).on('click', '.navbar-toggler-icon', function(e){
+  e.preventDefault();
+  $('.overlay').show();
+  $('.overlay').data('element', 'navbar-toggler-icon');
+});
