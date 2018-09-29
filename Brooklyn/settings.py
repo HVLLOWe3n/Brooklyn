48107702fdfcc3d42c83a1cbc97f6b74a42ae539
Roman Synovets <Roman.Synovets@gmail.com>
2018-09-29 17:07:18 +0300

    Debug mode disabled

--- Brooklyn/settings.py
+++ Brooklyn/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nc&@^=26@iwdg0t#v9x3b+9iw@%7c=%^ih#_1li!-z_#cc+xqx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []  # '.herokuapp.com', 'localhost'
 
